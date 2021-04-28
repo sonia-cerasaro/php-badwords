cdb413d574035ed65a1af231077fc8a3674d7af7
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-28 14:29:49 +0200

    php prima prova

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+    <?php
+      $nome = "Sonia";
+    ?>
+
+    <h1>
+      Ciao mi chiamo <?php echo $nome ?>
+    </h1>
+
+    
+
+  </body>
+</html>
